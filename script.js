6eb13397e5e433f789055ebb9d789dd363c0b1e5
julien <98946443+julien44830@users.noreply.github.com>
2022-08-13 10:22:33 +0200

    1er script

---
+++ script.js
@@ -0,0 +1,8 @@
+//window.alert('alert papa m\'aprend le javacripte');
+
+
+alert('Tom ce met au JavaScript')  // j'ai rajouté ce texte ;)
+
+const text = document.querySelector(`#h1`);
+
+console.log(text);
